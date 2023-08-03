ab99950e731b0235b989550178064007338d56e3
Dwisgolmog <rlaehdgus134@naver.com>
2023-08-03 17:01:16 +0900

    메인페이지가 까지 완성

--- storyai/src/App.js
+++ storyai/src/App.js
@@ -1,10 +1,21 @@
- import './App.css';
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Banner from './Components/Banner';
+import { Container } from '@mui/system';
+import Content from './Components/content';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <NavBar></NavBar>
+      <Banner></Banner>
+      <Container fixed>
+        <Content></Content>
+      </Container>
+      <Footer/>
+    </>
   );
 }
 
