574d9ecbcee1de09c23b2d9f989bb357c3568cd1
Dwisgolmog <rlaehdgus134@naver.com>
2023-08-03 20:46:56 +0900

    react-router-dom@6설치

--- storyai/src/App.js
+++ storyai/src/App.js
@@ -5,6 +5,7 @@ import Banner from './Components/Banner';
 import { Container } from '@mui/system';
 import Content from './Components/content';
 import Footer from './Components/Footer';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
