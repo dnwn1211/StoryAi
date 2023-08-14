7881c2285cb11e9a58d2f1d1c4ba2a4b9e5ce89a
dnwn1211 <dnwn3027@gmail.com>
2023-08-14 22:24:18 +0900

    StoryAi
    StoryAi-server

--- server.js/server.js
+++ server.js/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.listen(8080, function() {
     console.log('listening on 8080')
 })
 
+app.use(express.static(path.join(__dirname,'stroyai/build')))
+
 app.get('/story', function(a,r){
-  r.send('Story Ai')
+  r.sendFile(path.join(__dirname,'storyai/build/index.html'))
 })
